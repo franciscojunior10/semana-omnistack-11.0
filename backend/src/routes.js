fcccf2c366adb98da7db74f10f771153642e91c7
Francisco Junior <franciscojn49@gmail.com>
2020-03-27 14:06:23 -0300

    validação em todas as rotas (ok)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -7,17 +8,89 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.store);
+// Rotas session
+// Rota para criar uma session (validação de BODY)
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required(),
+    })
+}), SessionController.store);
 
-routes.post('/ongs', OngController.store);
+// Rotas ongs
+// Rota para criar uma ong (validação de BODY)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().pattern(/^[0-9]+$/, 'numbers').required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.store);
+
+// Rota para mostar as ongs cadastradas
 routes.get('/ongs', OngController.index);
 
-routes.post('/incidents', IncidentController.store);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
-routes.put('/incidents/:id', IncidentController.update);
+// Rotas incidents
+// Rota para criar um incidents (validação de BODY e HEADERS)
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number(),
+    }),
 
-routes.get('/profile', ProfileController.index);
-routes.get('/profile/:id', ProfileController.show);
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), IncidentController.store);
+
+// Rota para mostrar os incidents (validação de QUERY)
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.index);
+
+// Rota para deletar um incident (validação de PARAMS e HEADERS)
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    }),
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), IncidentController.delete);
+
+// Rota para atualizar um incident (validação de PARAMS e HEADERS)
+routes.put('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    }),
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), IncidentController.update);
+
+// Rotas profile
+// Rota para buscar todos os casos daquela ong
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+// Rota para buscar um caso especifico daquela ong
+routes.get('/profile/:id', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), ProfileController.show);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor Executando! ... http://localhost:${process.env.PORT}`);
